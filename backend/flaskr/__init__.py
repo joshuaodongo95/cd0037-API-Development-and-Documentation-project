9f62db2077252b2fedacea3d7ff3444b659dbaec
unknown <joshuaodongo95@gmail.com>
2022-11-18 14:20:55 +0300

    Add Get categories endpoint, get paginated questions endpoint

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -10,6 +10,15 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+def paginate_questions(request, selection):
+    page = request.args.get("page", 1 , type = int)
+    start = (page-1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
+
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__)
@@ -28,13 +37,24 @@ def create_app(test_config=None):
         response.headers.add("Access-Control-Allow-Methods", "GET,PATCH,POST,DELETE,OPTIONS")
         return response
     """
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    # Get all categories endpoint
+    @app.route('/categories')
+    @cross_origin()
+    def get_categories():
+        categories = Category.query.all()
+        formatted_categories = {category.id: category.type for category in categories}
 
+        return jsonify({
+                        'success':True,
+                        'categories':formatted_categories        
+                        })
+        
     """
-    @TODO:
+    @DONE:
     Create an endpoint to handle GET requests for questions,
     including pagination (every 10 questions).
     This endpoint should return a list of questions,
@@ -45,7 +65,24 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions')
+    def get_questions():
+        selection = Question.query.order_by(Question.id).all()
+        categories = Category.query.all()
 
+        current_questions = paginate_questions(request, selection)
+        formatted_categories = {category.id: category.type for category in categories}
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+                            'success':True,
+                            'questions':current_questions,
+                            'total_questions':len(Question.query.all()),
+                            'current_category':'',
+                            'categories':formatted_categories
+                        })
     """
     @TODO:
     Create an endpoint to DELETE question using a question ID.
